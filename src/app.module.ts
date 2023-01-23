5d285e09823cc32d45fd35cbad0dc8df9d57b0c3
RomanButsko <75183199+RomanButsko@users.noreply.github.com>
2023-01-23 15:56:08 +0300

    changed env

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,8 +16,8 @@ import { ConfigModule } from '@nestjs/config';
     ),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
